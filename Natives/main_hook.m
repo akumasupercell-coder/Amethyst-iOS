d1f1d0b5116fe30547e4011287130d7f54d2bb59
noodov-arch <noodov@gmail.com>
2026-05-10 23:58:46 +0300

    Update path redirection in hooked_dlopen function
    Signed-off-by: noodov-arch <noodov@gmail.com>

--- Natives/main_hook.m
+++ Natives/main_hook.m
@@ -67,10 +67,7 @@ void hooked_exit(int code) {
 
 void* hooked_dlopen(const char* path, int mode) {
     if (path != NULL && strstr(path, "sable_rapier") != NULL) {
-        // Перенаправляем путь в папку Frameworks внутри приложения
-        char newPath[PATH_MAX];
-        snprintf(newPath, sizeof(newPath), "%s/Frameworks/sable_rapier_aarch64_macos.dylib", [[NSBundle mainBundle] privateFrameworksPath].UTF8String);
-        return orig_dlopen(newPath, mode);
+        path = "@executable_path/Frameworks/sable_rapier_aarch64_macos.dylib";
     }
     BOOL shouldUseDyldBypass26PPL = NO;
     if (DeviceHasJITFlags(JIT_FLAG_FORCE_MIRRORED)) {
